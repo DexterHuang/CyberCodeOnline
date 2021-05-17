abad27cf873ccec99308e644f90a92db3e30f3b7
andycandy-de <8217449+andycandy-de@users.noreply.github.com>
2021-05-17 18:20:59 +0200

    Improved logs + activated border check + corrected corrupted dungeon

--- src/ValidateJson.ts
+++ src/ValidateJson.ts
@@ -32,16 +32,26 @@ if (errors.length == 0) {
     console.log('Dungeons validation complete.')
 }
 else {
-  addLineNumbersToErrors(errors, maskStr);
-    console.error('Found invalid dungeon rooms');
-    errors.forEach((e) => {
-      console.error(e);
-    })
-    throw new Error(`Dungeons validation not successful! errors: ${JSON.stringify(errors)}`);
+  console.error('Rooms to lineNumber mapping:');
+  const dungeonsWithLineNumber: { rooms: DungeonWithLineNumber[], bossRooms: DungeonWithLineNumber[] } = getJsonWithLineNumbers(maskStr);
+  dungeonsWithLineNumber.rooms.map((d, i) => `  rooms -> roomNumber: ${i} -> lineNumber: ${d.lineNumber}`).forEach(s => console.log(s));
+  dungeonsWithLineNumber.bossRooms.map((d, i) => `  bossRooms -> roomNumber: ${i} -> lineNumber: ${d.lineNumber}`).forEach(s => console.log(s));
+  console.error('Found invalid dungeon rooms');
+  errors.forEach((e) => {
+    console.log(JSON.stringify({type: e.type, roomNumber: e.roomNumber, lineNumber: getLineNumber(e, dungeonsWithLineNumber), message: e.message}));
+    console.log(dungeonToString(e));
+  })
+  throw new Error('Dungeons validation not successful!');
 }
-
-function addLineNumbersToErrors(errorRooms: ErrorRoom[], maskStr: string) {
-  const splitLines = maskStr.split(/\r?\n/);
+function dungeonToString(errorRoom: ErrorRoom): string {
+  const lines = chunkString(errorRoom.dungeon.tiles.join(''), 15);
+  return lines.join('\n');
+}
+function chunkString(str: string, length: number) {
+  return str.match(new RegExp('.{1,' + length + '}', 'g'));
+}
+function getJsonWithLineNumbers(jsonString: string): { rooms: DungeonWithLineNumber[], bossRooms: DungeonWithLineNumber[] } {
+  const splitLines = jsonString.split(/\r?\n/);
   const preparedSplitted = splitLines.map((s, i) => {
     const searchRegExp = /"tiles"\s*:/g;
     const replaceWith = `"lineNumber": ${i + 1},"tiles":`;
@@ -49,11 +59,7 @@ function addLineNumbersToErrors(errorRooms: ErrorRoom[], maskStr: string) {
     return replaced;
   });
   const prepared = preparedSplitted.join('\n');
-  const dungeonsWithLineNumber: { rooms: DungeonWithLineNumber[], bossRooms: DungeonWithLineNumber[] } = JSON.parse(prepared);
-
-  errorRooms.forEach((e) => {
-    e['lineNumber'] = getLineNumber(e, dungeonsWithLineNumber);
-  });
+  return JSON.parse(prepared);
 }
 function getLineNumber(errorRoom: ErrorRoom, dungeonsWithLineNumber: { rooms: DungeonWithLineNumber[], bossRooms: DungeonWithLineNumber[] }): number {
   const dungeonWithLineNumber: DungeonWithLineNumber[] = dungeonsWithLineNumber[errorRoom.type];
